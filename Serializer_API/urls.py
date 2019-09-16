cfd6d5b038b1c94539d478d23037b9aca72d3a1a
Roshan Pandey <pandeyroshan556@gmail.com>
2019-09-16 15:34:25 +0530

    Update urls.py

--- Serializer_API/urls.py
+++ Serializer_API/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from core.views import dataSetView
+from core.views import login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('data/',dataSetView.as_view())
+    path('data/',dataSetView.as_view()),
+    path('login/',login)
 ]
